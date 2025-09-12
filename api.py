ca9ab04a293773b617ab62b16e22830d7de68430
cunhavictor111 <victorac1@al.insper.edu.br>
2025-09-12 15:07:30 -0300

    banco de dados rodando

--- api.py
+++ api.py
@@ -4,12 +4,14 @@ import mysql.connector
 from mysql.connector import Error
 from dotenv import load_dotenv
 
+load_dotenv('.env')
+
 config = {
     'host': os.getenv('DB_HOST', 'localhost'),  # Obtém o host do banco de dados da variável de ambiente
     'user': os.getenv('DB_USER'),  # Obtém o usuário do banco de dados da variável de ambiente
     'password': os.getenv('DB_PASSWORD'),  # Obtém a senha do banco de dados da variável de ambiente
-    'database': os.getenv('DB_NAME', 'db_escola'),  # Obtém o nome do banco de dados da variável de ambiente
-    'port': int(os.getenv('DB_PORT', 3306)),  # Obtém a porta do banco de dados da variável de ambiente
+    'database': os.getenv('DB_NAME', 'imoveis'),  # Obtém o nome do banco de dados da variável de ambiente
+    'port': int(os.getenv('DB_PORT', 5000)),  # Obtém a porta do banco de dados da variável de ambiente
     'ssl_ca': os.getenv('SSL_CA_PATH')  # Caminho para o certificado SSL
 }
 
@@ -27,6 +29,15 @@ def connect_db():
 
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_imoveis():
+    conn = connect_db()
+    if conn is None:
+        resp = {"erro": "Erro ao conectar ao banco de dados"}
+        return resp, 500
+    resp = {"conectou": "conectou ao banco de dados"}
+    return resp, 200
 
 if __name__ == '__main__':
     app.run(debug=True)
